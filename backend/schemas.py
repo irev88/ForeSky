b7da1e8d027e8493aa3f010bdf37b00237bac4cb
irev88 <rev.syc088@gmail.com>
2025-09-05 18:48:17 +0800

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,13 +1,5 @@
-from typing import List, Optional
 from pydantic import BaseModel
-
-class TagBase(BaseModel):
-    name: str
-
-class Tag(TagBase):
-    id: int
-    class Config:
-        from_attributes = True
+from typing import List, Optional
 
 class NoteBase(BaseModel):
     title: str
@@ -17,8 +9,38 @@ class NoteBase(BaseModel):
 class NoteCreate(NoteBase):
     pass
 
+class TagBase(BaseModel):
+    name: str
+
+class Tag(TagBase):
+    id: int
+    class Config:
+        from_attributes = True
+
 class Note(NoteBase):
     id: int
     tags: List[Tag] = []
     class Config:
         from_attributes = True
+
+# User Schemas
+class UserBase(BaseModel):
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    notes: List[Note] = []
+    class Config:
+        from_attributes = True
+        
+# Token Schemas
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
